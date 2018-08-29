4f9c87aad8ab087e573223835af755e3a9a46baf
ruslan <ruslan>
2018-08-29 14:53:04 +0500

    second commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,9 @@
 $(function() {
 
   $(".roadmap__item").click(function() {
+  	$(".roadmap__list-row").removeClass('active');
+  	$(".roadmap__item").removeClass('active');
+  	$(this).parents(".roadmap__list-row").addClass('active');
   	$(this).addClass('active').siblings().removeClass('active');
   });
 
